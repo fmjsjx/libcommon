bc22fa64e5f0b2eaa83c76931ea1b9d018b906ec
fmjsjx <fmjsjx@163.com>
2020-12-25 17:04:43 +0800

    Update javadoc.

--- libcommon-util/src/main/java/com/github/fmjsjx/libcommon/util/NumberUtil.java
+++ libcommon-util/src/main/java/com/github/fmjsjx/libcommon/util/NumberUtil.java
@@ -1,7 +1,17 @@
 package com.github.fmjsjx.libcommon.util;
 
+/**
+ * Utility class for numbers.
+ */
 public class NumberUtil {
 
+    /**
+     * Converts and returns the given value as an {@code int}.
+     *
+     * @param value a number
+     * @param defaultValue the default value
+     * @return an {@code int}
+     */
     public static final int intValue(Number value, int defaultValue) {
         if (value == null) {
             return defaultValue;
@@ -9,6 +19,12 @@ public class NumberUtil {
         return value.intValue();
     }
 
+    /**
+     * Converts an returns the given value as an {@code int}.
+     *
+     * @param value a number
+     * @return an {@code int}
+     */
     public static final int intValue(Number value) {
         return intValue(value, 0);
     }
