e3c36c34eaab2c598ea26f5d3689a14965bba65f
fmjsjx <fmjsjx@163.com>
2021-01-12 17:55:00 +0800

    Move up libcommon-jdbc

--- libcommon-bom/build.gradle.kts
+++ libcommon-bom/build.gradle.kts
@@ -8,13 +8,13 @@ description = "libcommon/BOM"
 dependencies {
 	constraints {
         api(project(":libcommon-collection"))
+        api(project(":libcommon-jdbc"))
         api(project(":libcommon-json"))
         api(project(":libcommon-json-dsljson"))
         api(project(":libcommon-json-jackson2"))
         api(project(":libcommon-json-jsoniter"))
         api(project(":libcommon-redis"))
         api(project(":libcommon-util"))
-        api(project(":libcommon-jdbc"))
     }
 }
 
