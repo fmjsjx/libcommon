7e48b75cba2e82ec71cb0fa21a408e77f34f5ff7
fmjsjx <fmjsjx@163.com>
2021-01-12 17:45:38 +0800

    Add libcommon-jdbc to bom.

--- libcommon-bom/build.gradle.kts
+++ libcommon-bom/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
         api(project(":libcommon-json-jsoniter"))
         api(project(":libcommon-redis"))
         api(project(":libcommon-util"))
+        api(project(":libcommon-jdbc"))
     }
 }
 
