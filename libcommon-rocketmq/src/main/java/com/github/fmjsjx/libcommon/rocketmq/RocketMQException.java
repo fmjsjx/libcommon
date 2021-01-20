6deaf2f700d993c583b8a8981ae73be8b5197456
fmjsjx <fmjsjx@163.com>
2021-01-20 18:34:35 +0800

    Finish code for RocketMQUtil.

---
+++ libcommon-rocketmq/src/main/java/com/github/fmjsjx/libcommon/rocketmq/RocketMQException.java
@@ -0,0 +1,65 @@
+package com.github.fmjsjx.libcommon.rocketmq;
+
+/**
+ * Base exception for RocketMQ.
+ */
+public class RocketMQException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructs a new {@link RocketMQException} with {@code null} as its detail
+     * message. The cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     */
+    public RocketMQException() {
+        super();
+    }
+
+    /**
+     * Constructs a new {@link RocketMQException} with the specified detail message
+     * and cause.
+     * <p>
+     * Note that the detail message associated with {@code cause} is <i>not</i>
+     * automatically incorporated in this exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval by the
+     *                {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method). (A {@code null} value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     */
+    public RocketMQException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new {@link RocketMQException} with the specified detail message.
+     * The cause is not initialized, and may subsequently be initialized by a call
+     * to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for later
+     *                retrieval by the {@link #getMessage()} method.
+     */
+    public RocketMQException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new {@link RocketMQException} with the specified cause and a
+     * detail message of {@code (cause==null ? null : cause.toString())} (which
+     * typically contains the class and detail message of {@code cause}). This
+     * constructor is useful for exceptions that are little more than wrappers for
+     * other {@link Throwable}s (for example,
+     * {@link java.security.PrivilegedActionException}).
+     *
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link #getCause()} method). (A {@code null} value is permitted,
+     *              and indicates that the cause is nonexistent or unknown.)
+     */
+    public RocketMQException(Throwable cause) {
+        super(cause);
+    }
+
+}
---
+++ libcommon-rocketmq/src/main/java/com/github/fmjsjx/libcommon/rocketmq/TooManyRetryException.java
@@ -0,0 +1,66 @@
+package com.github.fmjsjx.libcommon.rocketmq;
+
+import java.util.Collection;
+
+/**
+ * Exception for too many retry.
+ */
+public class TooManyRetryException extends RocketMQException {
+
+    private static final long serialVersionUID = 1L;
+
+    private final Iterable<Throwable> causes;
+    private final int retryTimes;
+
+    /**
+     * Constructs a new {@link RocketMQException} with the specified detail message,
+     * cause and causes.
+     * 
+     * @param message the detail message
+     * @param cause   the cause
+     * @param causes  the history causes
+     */
+    public TooManyRetryException(String message, Throwable cause, Collection<Throwable> causes) {
+        super(message, cause);
+        this.causes = causes;
+        this.retryTimes = causes.size();
+    }
+
+    /**
+     * Constructs a new {@link RocketMQException} with the specified cause and
+     * causes.
+     * 
+     * @param cause  the cause
+     * @param causes the history causes
+     */
+    public TooManyRetryException(Throwable cause, Collection<Throwable> causes) {
+        super(cause);
+        this.causes = causes;
+        this.retryTimes = causes.size();
+    }
+
+    /**
+     * Returns the history causes.
+     * 
+     * @return the history causes
+     */
+    public Iterable<Throwable> getCauses() {
+        return causes;
+    }
+
+    /**
+     * Returns the retry times.
+     * 
+     * @return the retry times
+     */
+    public int getRetryTimes() {
+        return retryTimes;
+    }
+
+    @Override
+    public String getLocalizedMessage() {
+        var msg = getMessage();
+        return msg == null ? "retry times " + retryTimes : msg + ", retry times " + retryTimes;
+    }
+
+}
