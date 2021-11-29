bb4a4c3f5d76e220f441eb1565c0911753255b0a
fmjsjx <fmjsjx@163.com>
2021-11-29 10:43:56 +0800

    Upgrade netty to 4.1.70.Final

--- buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.69.Final"))
+    api(platform("io.netty:netty-bom:4.1.70.Final"))
     // junit-bom
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     // jackson2-bom
