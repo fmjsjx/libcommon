22eb6cb918cae82eea7d712143852dd3fb9f67f0
fmjsjx <fmjsjx@163.com>
2022-09-13 14:55:20 +0800

    Upgrade dependencies
    ```
    netty       4.1.74.Final  -> 4.1.81.Final
    junit       5.8.2         -> 5.9.0
    jackson     2.13.1        -> 2.13.4
    rocketmq    4.9.2         -> 4.9.4
    logback     1.2.10        -> 1.2.11
    lettuce     6.1.6.RELEASE -> 6.2.0.RELEASE
    ons-client  2.0.0.Final   -> 2.0.3.Final
    mongodb     4.4.1         -> 4.7.1
    jruby       9.3.2.0       -> 9.3.7.0
    log4j       2.17.1        -> 2.18.0
    ```

--- buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
@@ -11,34 +11,36 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.74.Final"))
+    api(platform("io.netty:netty-bom:4.1.81.Final"))
     // junit-bom
-    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation(platform("org.junit:junit-bom:5.9.0"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.13.1"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.13.4"))
     // rocketmq
-    api(platform("org.apache.rocketmq:rocketmq-all:4.9.2"))
+    api(platform("org.apache.rocketmq:rocketmq-all:4.9.4"))
 
     constraints {
         implementation("org.slf4j:slf4j-api:1.7.36")
-        implementation("ch.qos.logback:logback-classic:1.2.10")
-        api("io.lettuce:lettuce-core:6.1.6.RELEASE")
+        implementation("ch.qos.logback:logback-classic:1.2.11")
+        api("io.lettuce:lettuce-core:6.2.0.RELEASE")
         api("com.dslplatform:dsl-json-java8:1.9.9")
         api("com.jsoniter:jsoniter:0.9.23")
-        api("com.aliyun.openservices:ons-client:2.0.0.Final")
-        api("org.mongodb:bson:4.4.1")
-        api("org.mongodb:mongodb-driver-core:4.4.1")
-        api("org.mongodb:mongodb-driver-sync:4.4.1")
-        api("org.mongodb:mongodb-driver-reactivestreams:4.4.1")
-        api("org.mongodb:mongodb-driver-legacy:4.4.1")
-        implementation("org.jruby:jruby-complete:9.3.2.0")
-        implementation("org.jruby:jruby:9.3.2.0")
-        implementation("org.jruby:jruby-core:9.3.2.0")
-        implementation("org.jruby:jruby-stdlib:9.3.2.0")
-        implementation("org.yaml:snakeyaml:1.30")
+        api("com.aliyun.openservices:ons-client:2.0.3.Final")
+        val mongodbVersion = "4.7.1"
+        api("org.mongodb:bson:$mongodbVersion")
+        api("org.mongodb:mongodb-driver-core:$mongodbVersion")
+        api("org.mongodb:mongodb-driver-sync:$mongodbVersion")
+        api("org.mongodb:mongodb-driver-reactivestreams:$mongodbVersion")
+        api("org.mongodb:mongodb-driver-legacy:$mongodbVersion")
+        val jrubyVersion = "9.3.7.0"
+        implementation("org.jruby:jruby-complete:$jrubyVersion")
+        implementation("org.jruby:jruby:$jrubyVersion")
+        implementation("org.jruby:jruby-core:$jrubyVersion")
+        implementation("org.jruby:jruby-stdlib:$jrubyVersion")
+        implementation("org.yaml:snakeyaml:1.32")
     }
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.17.1"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.18.0"))
 
 }
 
