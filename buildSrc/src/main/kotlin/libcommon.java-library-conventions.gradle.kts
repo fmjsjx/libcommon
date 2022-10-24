0274001215c1caf54fa4ed19bfa30e166c9691de
fmjsjx <fmjsjx@163.com>
2022-10-24 18:04:33 +0800

    Upgrade dependencies
    ```
    netty      4.1.82.Final  > 4.1.84.Final
    junit      5.9.0         > 5.9.1
    jackson2   2.13.4        > 2.13.4.20221013
    lettuce    6.2.0.RELEASE > 6.2.1.RELEASE
    mongodb    4.7.1         > 4.7.2
    jruby      9.3.7.0       > 9.3.8.0
    snakeyaml  1.32          > 1.33
    log4j      2.18.0        > 2.19.0
    ```

--- buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
@@ -11,36 +11,36 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.82.Final"))
+    api(platform("io.netty:netty-bom:4.1.84.Final"))
     // junit-bom
-    testImplementation(platform("org.junit:junit-bom:5.9.0"))
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.13.4"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.13.4.20221013"))
     // rocketmq
     api(platform("org.apache.rocketmq:rocketmq-all:4.9.4"))
 
     constraints {
         implementation("org.slf4j:slf4j-api:1.7.36")
         implementation("ch.qos.logback:logback-classic:1.2.11")
-        api("io.lettuce:lettuce-core:6.2.0.RELEASE")
+        api("io.lettuce:lettuce-core:6.2.1.RELEASE")
         api("com.dslplatform:dsl-json-java8:1.9.9")
         api("com.jsoniter:jsoniter:0.9.23")
-        api("com.aliyun.openservices:ons-client:2.0.3.Final")
-        val mongodbVersion = "4.7.1"
+        api("com.aliyun.openservices:ons-client:2.0.4.Final")
+        val mongodbVersion = "4.7.2"
         api("org.mongodb:bson:$mongodbVersion")
         api("org.mongodb:mongodb-driver-core:$mongodbVersion")
         api("org.mongodb:mongodb-driver-sync:$mongodbVersion")
         api("org.mongodb:mongodb-driver-reactivestreams:$mongodbVersion")
         api("org.mongodb:mongodb-driver-legacy:$mongodbVersion")
-        val jrubyVersion = "9.3.7.0"
+        val jrubyVersion = "9.3.8.0"
         implementation("org.jruby:jruby-complete:$jrubyVersion")
         implementation("org.jruby:jruby:$jrubyVersion")
         implementation("org.jruby:jruby-core:$jrubyVersion")
         implementation("org.jruby:jruby-stdlib:$jrubyVersion")
-        implementation("org.yaml:snakeyaml:1.32")
+        implementation("org.yaml:snakeyaml:1.33")
     }
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.18.0"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.19.0"))
 
 }
 
