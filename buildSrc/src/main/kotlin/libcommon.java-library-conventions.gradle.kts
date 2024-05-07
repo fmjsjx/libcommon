0b5e0cc712f9b48d8a729dda7a5f3d531c776981
fmjsjx <fmjsjx@163.com>
2024-05-07 14:31:21 +0800

    Upgrade dependencies
    ```
    netty       4.1.107.Final > 4.1.109.Final
    jackson2    2.16.1        > 2.17.1
    rocketmq    5.1.4         > 5.2.0
    slf4j       2.0.12        > 2.0.13
    logback     1.5.3         > 1.5.6
    lettuce     6.3.1.RELEASE > 6.3.2.RELEASE
    ons-client  2.0.6.Final   > 2.0.7.Final
    fastjson2   2.0.47        > 2.0.49
    mongodb     5.0.0         > 5.1.0
    jruby       9.4.5.0       > 9.4.7.0
    snakeyaml   2.0           > 2.1
    prometheus  1.1.0         > 1.2.1
    log4j2      2.23.0        > 2.23.1
    ```

--- buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
@@ -11,15 +11,15 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.1.107.Final"))
+    api(platform("io.netty:netty-bom:4.1.109.Final"))
     // junit-bom
     testImplementation(platform("org.junit:junit-bom:5.10.2"))
     // mockito
     testImplementation(platform("org.mockito:mockito-bom:5.11.0"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.16.1"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.17.1"))
     // rocketmq
-    api(platform("org.apache.rocketmq:rocketmq-all:5.1.4"))
+    api(platform("org.apache.rocketmq:rocketmq-all:5.2.0"))
     // kotlin coroutines
     api(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.8.0"))
     // prometheus
@@ -28,28 +28,28 @@ dependencies {
     api(platform("org.jetbrains.kotlin:kotlin-bom:1.9.0"))
 
     constraints {
-        implementation("org.slf4j:slf4j-api:2.0.12")
-        implementation("ch.qos.logback:logback-classic:1.5.3")
-        api("io.lettuce:lettuce-core:6.3.1.RELEASE")
+        implementation("org.slf4j:slf4j-api:2.0.13")
+        implementation("ch.qos.logback:logback-classic:1.5.6")
+        api("io.lettuce:lettuce-core:6.3.2.RELEASE")
         api("com.dslplatform:dsl-json-java8:1.10.0")
         api("com.jsoniter:jsoniter:0.9.23")
-        api("com.aliyun.openservices:ons-client:2.0.6.Final")
-        val fastjson2Version = "2.0.47"
+        api("com.aliyun.openservices:ons-client:2.0.7.Final")
+        val fastjson2Version = "2.0.49"
         api("com.alibaba.fastjson2:fastjson2:$fastjson2Version")
         api("com.alibaba.fastjson2:fastjson2-kotlin:$fastjson2Version")
-        val mongodbVersion = "5.0.0"
+        val mongodbVersion = "5.1.0"
         api("org.mongodb:bson:$mongodbVersion")
         api("org.mongodb:mongodb-driver-core:$mongodbVersion")
         api("org.mongodb:mongodb-driver-sync:$mongodbVersion")
         api("org.mongodb:mongodb-driver-reactivestreams:$mongodbVersion")
         api("org.mongodb:mongodb-driver-legacy:$mongodbVersion")
-        val jrubyVersion = "9.4.5.0"
+        val jrubyVersion = "9.4.7.0"
         implementation("org.jruby:jruby-complete:$jrubyVersion")
         implementation("org.jruby:jruby:$jrubyVersion")
         implementation("org.jruby:jruby-core:$jrubyVersion")
         implementation("org.jruby:jruby-stdlib:$jrubyVersion")
-        implementation("org.yaml:snakeyaml:2.0")
-        val prometheusVersion = "1.1.0"
+        implementation("org.yaml:snakeyaml:2.2")
+        val prometheusVersion = "1.2.1"
         api("io.prometheus:prometheus-metrics-core:$prometheusVersion")
         api("io.prometheus:prometheus-metrics-model:$prometheusVersion")
         api("io.prometheus:prometheus-metrics-tracer:$prometheusVersion")
@@ -57,7 +57,7 @@ dependencies {
         api("io.prometheus:prometheus-metrics-instrumentation-jvm:$prometheusVersion")
     }
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.23.0"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.23.1"))
 
 }
 
