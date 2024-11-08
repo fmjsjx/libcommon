cdf2976150ea2ff203bf3716644b203969298b5c
fmjsjx <fmjsjx@163.com>
2024-11-08 17:49:11 +0800

    Supplements `KDoc`s on module `libcommon-util-kotlin`

--- libcommon-util-kotlin/src/main/kotlin/com/github/fmjsjx/libcommon/util/TextExtensions.kt
+++ libcommon-util-kotlin/src/main/kotlin/com/github/fmjsjx/libcommon/util/TextExtensions.kt
@@ -4,8 +4,36 @@ import java.net.URLDecoder
 import java.net.URLEncoder
 import java.nio.charset.Charset
 
+/**
+ * Returns `true` if the [CharSequence] contains only Unicode digits, otherwise `false`.
+ *
+ * A decimal point is not a Unicode digit and returns `false`.
+ *
+ * `null` will return `false`. An empty CharSequence (length()=0) will return `false`.
+ *
+ * @author MJ Fang
+ * @since 3.10
+ */
 val <T : CharSequence> T.isNumberic: Boolean get() = StringUtil.isNumberic(toString())
 
+/**
+ * Translates a [CharSequence] into `application/x-www-form-urlencoded` format using a specific [Charset].
+ *
+ * @param charset the given charset, the default value is [Charsets.UTF_8].
+ * @return the translated [String].
+ *
+ * @author MJ Fang
+ * @since 3.10
+ */
 fun <T : CharSequence> T.urlEncode(charset: Charset = Charsets.UTF_8): String = URLEncoder.encode(toString(), charset)
 
+/**
+ * Decodes an `application/x-www-form-urlencoded` [CharSequence] using a specific [Charset].
+ *
+ * @param charset the given charset, the default value is [Charsets.UTF_8].
+ * @return the newly decoded [String].
+ *
+ * @author MJ Fang
+ * @since 3.10
+ */
 fun <T : CharSequence> T.urlDecode(charset: Charset = Charsets.UTF_8): String = URLDecoder.decode(toString(), charset)
