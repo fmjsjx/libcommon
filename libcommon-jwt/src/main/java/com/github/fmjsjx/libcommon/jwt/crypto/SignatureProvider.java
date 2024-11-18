8d06604bf143544ec4bc33b83217b9aa0e4e8945
fmjsjx <fmjsjx@163.com>
2024-11-18 17:28:58 +0800

    Use thread local signature only in verify methods

--- libcommon-jwt/src/main/java/com/github/fmjsjx/libcommon/jwt/crypto/SignatureProvider.java
+++ libcommon-jwt/src/main/java/com/github/fmjsjx/libcommon/jwt/crypto/SignatureProvider.java
@@ -75,14 +75,15 @@ public interface SignatureProvider extends SecureProvider {
      *                  going to be verified
      * @return a {@link Signature} object that implements the specified
      * signature  algorithm and be initialized for verification
-     * @throws NoSuchAlgorithmException if no {@code Provider} supports a
-     *                                  {@code KeyFactorySpi}
-     *                                  implementation for the specified
-     *                                  algorithm
+     * @throws NoSuchAlgorithmException           if no {@code Provider}
+     *                                            supports a
+     *                                            {@code KeyFactorySpi}
+     *                                            implementation for the
+     *                                            specified algorithm
      * @throws InvalidAlgorithmParameterException if the given parameters
      *                                            are inappropriate for
      *                                            this signature engine
-     * @throws InvalidKeyException      if the key is invalid
+     * @throws InvalidKeyException                if the key is invalid
      */
     default Signature getInstance(PublicKey publicKey) throws NoSuchAlgorithmException,
             InvalidAlgorithmParameterException, InvalidKeyException {
@@ -100,14 +101,15 @@ public interface SignatureProvider extends SecureProvider {
      *                   is going to be generated
      * @return a {@link Signature} object that implements the specified
      * signature  algorithm and be initialized for verification
-     * @throws NoSuchAlgorithmException if no {@code Provider} supports a
-     *                                  {@code KeyFactorySpi}
-     *                                  implementation for the specified
-     *                                  algorithm
+     * @throws NoSuchAlgorithmException           if no {@code Provider}
+     *                                            supports a
+     *                                            {@code KeyFactorySpi}
+     *                                            implementation for the
+     *                                            specified algorithm
      * @throws InvalidAlgorithmParameterException if the given parameters
      *                                            are inappropriate for
      *                                            this signature engine
-     * @throws InvalidKeyException      if the key is invalid
+     * @throws InvalidKeyException                if the key is invalid
      */
     default Signature getInstance(PrivateKey privateKey) throws NoSuchAlgorithmException,
             InvalidAlgorithmParameterException, InvalidKeyException {
@@ -125,27 +127,62 @@ public interface SignatureProvider extends SecureProvider {
      * @param signature the signature bytes to be verified
      * @return {@code true} if the signature was verified, {@code false}
      * if not
-     * @throws NoSuchAlgorithmException if no {@code Provider} supports a
-     *                                  {@code KeyFactorySpi}
-     *                                  implementation for the specified
-     *                                  algorithm
+     * @throws NoSuchAlgorithmException           if no {@code Provider}
+     *                                            supports a
+     *                                            {@code KeyFactorySpi}
+     *                                            implementation for the
+     *                                            specified algorithm
      * @throws InvalidAlgorithmParameterException if the given parameters
      *                                            are inappropriate for
      *                                            this signature engine
-     * @throws InvalidKeyException      if the key is invalid
-     * @throws SignatureException       if this signature object is not
-     *                                  initialized properly, the
-     *                                  passed-in signature is
-     *                                  improperly encoded or of the wrong
-     *                                  type, if this signature algorithm
-     *                                  is unable to process the input
-     *                                  data provided, etc
+     * @throws InvalidKeyException                if the key is invalid
+     * @throws SignatureException                 if this signature
+     *                                            object is not
+     *                                            initialized properly,
+     *                                            the passed-in signature
+     *                                            is improperly encoded
+     *                                            or of the wrong type,
+     *                                            if this signature
+     *                                            algorithm is unable to
+     *                                            process the input data
+     *                                            provided, etc.
      */
     default boolean verify(PublicKey publicKey, byte[] data, byte[] signature) throws NoSuchAlgorithmException,
             InvalidAlgorithmParameterException, InvalidKeyException, SignatureException {
         var sig = getInstance(publicKey);
         sig.update(data);
         return sig.verify(signature);
+    }
+
+    /**
+     * Returns the signature bytes of the data.
+     *
+     * @param privateKey the private key of the identity whose signature is going
+     *                   to be generated
+     * @param data       the data to be signed
+     * @return the signature bytes of the signing operation's result
+     * @throws NoSuchAlgorithmException           if no {@code Provider}
+     *                                            supports a
+     *                                            {@code KeyFactorySpi}
+     *                                            implementation for the
+     *                                            specified algorithm
+     * @throws InvalidAlgorithmParameterException if the given parameters
+     *                                            are inappropriate for
+     *                                            this signature engine
+     * @throws InvalidKeyException                if the key is invalid
+     * @throws SignatureException                 if this signature
+     *                                            object is not
+     *                                            initialized properly,
+     *                                            or if this signature
+     *                                            algorithm is unable to
+     *                                            process the input data
+     *                                            provided
+     */
+    default byte[] sign(PrivateKey privateKey, byte[] data) throws NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException, SignatureException {
+        var sig = getInstance(privateKey);
+        sig.update(data);
+        return sig.sign();
     }
 
 }
