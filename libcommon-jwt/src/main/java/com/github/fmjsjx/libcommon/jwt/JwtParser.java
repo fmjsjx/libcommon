ed1c3dcd24f1d8a0748cb846425fdc70a60db1d5
fmjsjx <fmjsjx@163.com>
2024-11-20 18:48:04 +0800

    Add part implementations of `JwtParser.Builder`

--- libcommon-jwt/src/main/java/com/github/fmjsjx/libcommon/jwt/JwtParser.java
+++ libcommon-jwt/src/main/java/com/github/fmjsjx/libcommon/jwt/JwtParser.java
@@ -1,6 +1,13 @@
 package com.github.fmjsjx.libcommon.jwt;
 
+import com.github.fmjsjx.libcommon.jwt.crypto.CryptoAlgorithm;
+import com.github.fmjsjx.libcommon.jwt.crypto.JwsCryptoAlgorithm;
+import com.github.fmjsjx.libcommon.jwt.exception.InvalidKeyException;
 import com.github.fmjsjx.libcommon.jwt.exception.JwtException;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Objects;
 
 /**
  * A parser parses {@link Jwt}s.
@@ -9,6 +16,10 @@ import com.github.fmjsjx.libcommon.jwt.exception.JwtException;
  * @since 3.10
  */
 public interface JwtParser {
+
+    static Builder builder() {
+        return new Builder();
+    }
 
     /**
      * Parse and returns the {@link Jwt} from the specified JWT string
@@ -21,5 +32,86 @@ public interface JwtParser {
      *                      rejected
      */
     Jwt parse(String jwt) throws JwtException;
+
+    /**
+     * The builder builds {@link JwtParser}s.
+     */
+    final class Builder {
+
+        private CryptoAlgorithm singleAlgorithm;
+        private Key singleKey;
+        private JsonRepresentedFactory<?> jsonRepresentedFactory;
+        private boolean allowExpired;
+        private long allowedClockSkewSeconds;
+
+        public Builder algorithm(CryptoAlgorithm singleAlgorithm) {
+            this.singleAlgorithm = singleAlgorithm;
+            return this;
+        }
+
+        public Builder key(Key singleKey) {
+            this.singleKey = singleKey;
+            return this;
+        }
+
+        public Builder fixedJsonRepresentedFactory(JsonRepresentedFactory<?> jsonRepresentedFactory) {
+            this.jsonRepresentedFactory = jsonRepresentedFactory;
+            return this;
+        }
+
+        private JsonRepresentedFactory<?> fixedJsonRepresentedFactory() {
+            return Objects.requireNonNullElseGet(jsonRepresentedFactory, Fastjson2JsonRepresented::getFactory);
+        }
+
+        public Builder allowExpired() {
+            return allowExpired(true);
+        }
+
+        public Builder disallowExpired() {
+            return allowExpired(false);
+        }
+
+        private Builder allowExpired(boolean allowExpired) {
+            this.allowExpired = allowExpired;
+            return this;
+        }
+
+        public Builder allowedClockSkewSeconds(long allowedClockSkewSeconds) {
+            this.allowedClockSkewSeconds = allowedClockSkewSeconds;
+            return this;
+        }
+
+        public JwtParser build() {
+
+            var singleKey = this.singleKey;
+            var singleAlgorithm = this.singleAlgorithm;
+            if (singleKey != null) {
+                if (singleAlgorithm != null) {
+                    // now only support JWS
+                    var algorithm = (JwsCryptoAlgorithm) singleAlgorithm;
+                    if (algorithm.isMac()) {
+                        if (singleKey instanceof SecretKey secretKey) {
+                            return new SimpleMacJwtParser(algorithm, secretKey, fixedJsonRepresentedFactory(), allowExpired, allowedClockSkewSeconds);
+                        } else {
+                            throw new InvalidKeyException("The key expected be a secret key but was " + singleKey.getClass());
+                        }
+                    } else {
+                        return new SimpleSignatureJwtParser(algorithm, singleKey, fixedJsonRepresentedFactory(), allowExpired, allowedClockSkewSeconds);
+                    }
+                } else {
+                    if (singleKey instanceof SecretKey secretKey) {
+                        var algorithm = (JwsCryptoAlgorithm) CryptoAlgorithm.getInstanceByJcaName(secretKey.getAlgorithm());
+                        return new SimpleMacJwtParser(algorithm, secretKey, fixedJsonRepresentedFactory(), allowExpired, allowedClockSkewSeconds);
+                    } else {
+                        throw new IllegalArgumentException("Missing JWT cryptographic algorithm");
+                    }
+                }
+            }
+            throw new IllegalArgumentException("Missing JWKS key");
+        }
+
+        private Builder() {
+        }
+    }
 
 }
