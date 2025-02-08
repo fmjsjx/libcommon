911e3188c55416fd564c8a88d752833d72db5127
fmjsjx <fmjsjx@163.com>
2025-02-08 18:04:27 +0800

    Fill unit tests (#112)
    * Fill unit tests
    
    * Fill unit tests
    
    * Fill unit tests

---
+++ libcommon-r2dbc/src/test/java/com/github/fmjsjx/libcommon/r2dbc/PersistentColumnInfoTests.java
@@ -0,0 +1,54 @@
+package com.github.fmjsjx.libcommon.r2dbc;
+
+import static com.github.fmjsjx.libcommon.r2dbc.PersistentColumnInfo.builder;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+public class PersistentColumnInfoTests {
+
+    @Test
+    public void testBuilder() {
+        var builder = builder();
+        assertNotNull(builder);
+        try {
+            builder.build();
+            fail("Should throw IllegalArgumentException");
+        } catch (Exception e) {
+            assertEquals("entityInfo must not be null", e.getMessage());
+        }
+        PersistentEntityInfo<?> mockEntityInfo = mock();
+        builder.entityInfo(mockEntityInfo);
+        try {
+            builder.build();
+            fail("Should throw IllegalArgumentException");
+        } catch (Exception e) {
+            assertEquals("columnName must not be null", e.getMessage());
+        }
+        builder.columnName("test");
+        try {
+            builder.build();
+            fail("Should throw IllegalArgumentException");
+        } catch (Exception e) {
+            assertEquals("valueGetter must not be null", e.getMessage());
+        }
+        Function<?, Object> mockValueGetter = mock();
+        builder.valueGetter(mockValueGetter);
+        var info = builder.build();
+        assertNotNull(info);
+        assertEquals(mockEntityInfo, info.getEntityInfo());
+        assertEquals("test", info.getColumnName());
+        assertEquals(mockValueGetter, info.getValueGetter());
+        assertFalse(info.isInsertOnly());
+        info = builder.insertOnly(true).build();
+        assertNotNull(info);
+        assertEquals(mockEntityInfo, info.getEntityInfo());
+        assertEquals("test", info.getColumnName());
+        assertEquals(mockValueGetter, info.getValueGetter());
+        assertTrue(info.isInsertOnly());
+    }
+
+}
