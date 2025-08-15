47d74de7a07a4c94851e612da71f0474127cfb06
fmjsjx <fmjsjx@163.com>
2025-08-15 13:52:29 +0800

    Fix wrong generic type on RedisExtension

--- libcommon-redis-kotlin/src/test/kotlin/com/github/fmjsjx/libcommon/redis/RedisExtensionTests.kt
+++ libcommon-redis-kotlin/src/test/kotlin/com/github/fmjsjx/libcommon/redis/RedisExtensionTests.kt
@@ -2,6 +2,7 @@ package com.github.fmjsjx.libcommon.redis
 
 import io.lettuce.core.RedisFuture
 import io.lettuce.core.api.async.RedisAsyncCommands
+import io.lettuce.core.cluster.api.async.RedisClusterAsyncCommands
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
@@ -17,6 +18,7 @@ import java.util.concurrent.CompletableFuture
 class RedisExtensionTests {
 
     private val redisAsyncCommands: RedisAsyncCommands<String, String> = mockk()
+    private val redisClusterAsyncCommands: RedisClusterAsyncCommands<String, String> = mockk()
 
     @AfterEach
     fun tearDown() {
@@ -31,12 +33,14 @@ class RedisExtensionTests {
         every { nil.toCompletableFuture() } returns CompletableFuture.completedFuture(null)
         every { redisAsyncCommands.set(any(), any(), any()) } returns ok
         every { redisAsyncCommands.set("nil", any(), any()) } returns nil
+        every { redisClusterAsyncCommands.set(any(), any(), any()) } returns ok
+        every { redisClusterAsyncCommands.set("nil", any(), any()) } returns nil
 
         runBlocking {
             assertTrue(redisAsyncCommands.tryLock("key", "value", 5, 10_000))
-        }
-        runBlocking {
             assertFalse(redisAsyncCommands.tryLock("nil", "value", 5, -1))
+            assertTrue(redisClusterAsyncCommands.tryLock("key", "value", 5, 10_000))
+            assertFalse(redisClusterAsyncCommands.tryLock("nil", "value", 5, -1))
         }
     }
 
