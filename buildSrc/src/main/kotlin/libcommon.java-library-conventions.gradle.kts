dd66de05567b251a99618508a542a5603023c797
fmjsjx <fmjsjx@163.com>
2025-12-03 14:28:56 +0800

    Upgrade dependencies
    ```
    netty        4.2.6.Final   > 4.2.7.Final
    junit        5.13.4        > 6.0.1
    mockito      5.19.0        > 5.20.0
    jackson2     2.20.0        > 2.20.1
    prometheus   1.4.1         > 1.4.3
    spring-boot  3.5.5         > 4.0.0
    logback      1.5.18        > 1.5.21
    lettuce      6.8.1.RELEASE > 7.1.0.RELEASE
    fastjson2    2.0.58        > 2.0.60
    mongodb      5.6.0         > 5.6.1
    log4j2       2.25.1        > 2.25.2
    ```

--- buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
@@ -12,13 +12,13 @@ repositories {
 
 dependencies {
     // netty-bom
-    api(platform("io.netty:netty-bom:4.2.6.Final"))
+    api(platform("io.netty:netty-bom:4.2.7.Final"))
     // junit-bom
-    testImplementation(platform("org.junit:junit-bom:5.13.4"))
+    testImplementation(platform("org.junit:junit-bom:6.0.1"))
     // mockito
-    testImplementation(platform("org.mockito:mockito-bom:5.19.0"))
+    testImplementation(platform("org.mockito:mockito-bom:5.20.0"))
     // jackson2-bom
-    api(platform("com.fasterxml.jackson:jackson-bom:2.20.0"))
+    api(platform("com.fasterxml.jackson:jackson-bom:2.20.1"))
     // jackson3-bom
     api(platform("tools.jackson:jackson-bom:3.0.3"))
     // rocketmq
@@ -27,23 +27,23 @@ dependencies {
     api(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.10.2"))
     // prometheus
     api(platform("io.prometheus:simpleclient_bom:0.16.0"))
-    api(platform("io.prometheus:prometheus-metrics-bom:1.4.1"))
+    api(platform("io.prometheus:prometheus-metrics-bom:1.4.3"))
     // kotlin
     api(platform("org.jetbrains.kotlin:kotlin-bom:2.2.0"))
     // spring boot
-    api(platform("org.springframework.boot:spring-boot-dependencies:3.5.5"))
+    api(platform("org.springframework.boot:spring-boot-dependencies:4.0.0"))
 
     constraints {
         implementation("org.slf4j:slf4j-api:2.0.17")
-        implementation("ch.qos.logback:logback-classic:1.5.18")
-        api("io.lettuce:lettuce-core:6.8.1.RELEASE")
+        implementation("ch.qos.logback:logback-classic:1.5.21")
+        api("io.lettuce:lettuce-core:7.1.0.RELEASE")
         api("com.dslplatform:dsl-json-java8:1.10.0")
         api("com.jsoniter:jsoniter:0.9.23")
         api("com.aliyun.openservices:ons-client:2.0.8.Final")
-        val fastjson2Version = "2.0.58"
+        val fastjson2Version = "2.0.60"
         api("com.alibaba.fastjson2:fastjson2:$fastjson2Version")
         api("com.alibaba.fastjson2:fastjson2-kotlin:$fastjson2Version")
-        val mongodbVersion = "5.6.0"
+        val mongodbVersion = "5.6.1"
         api("org.mongodb:bson:$mongodbVersion")
         api("org.mongodb:mongodb-driver-core:$mongodbVersion")
         api("org.mongodb:mongodb-driver-sync:$mongodbVersion")
@@ -60,7 +60,7 @@ dependencies {
         api("com.google.code.findbugs:jsr305:3.0.2")
     }
     // log4j2
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.25.1"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:2.25.2"))
 
 }
 
