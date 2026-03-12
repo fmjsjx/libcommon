68ba44ef3cd88884c6e7025c1f4ef788a07ee07a
fmjsjx <fmjsjx@163.com>
2026-03-12 10:26:51 +0800

    [Chore] Bump JUnit to 6.0.3

--- buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
@@ -16,7 +16,7 @@ dependencies {
     // mongodb-driver-bom
     implementation(platform("org.mongodb:mongodb-driver-bom:5.6.4"))
     // junit-bom
-    testImplementation(platform("org.junit:junit-bom:6.0.2"))
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
     // mockito
     testImplementation(platform("org.mockito:mockito-bom:5.21.0"))
     // jackson2-bom
