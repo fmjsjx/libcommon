69a8ef99b7b2c9f76ee31f17fa16335105455dc0
MJ Fang <fmjsjx@163.com>
2026-08-11 20:24:27 +0800

    [Chore] Bump String Boot to version 4.0.7

--- buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/libcommon.java-library-conventions.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     // Kotlin
     api(platform("org.jetbrains.kotlin:kotlin-bom:2.4.10"))
     // Spring Boot
-    api(platform("org.springframework.boot:spring-boot-dependencies:4.0.6"))
+    api(platform("org.springframework.boot:spring-boot-dependencies:4.0.7"))
 
     constraints {
         implementation("org.slf4j:slf4j-api:2.0.17")
