2484e07fb8065556c7cafae63ace4e5e3add0073
MJ Fang <fmjsjx@163.com>
2026-08-13 13:23:17 +0800

    [Chore] Migrate module libcommon-jwt-kotlin to libcommon-jwt

--- libcommon-bom/build.gradle.kts
+++ libcommon-bom/build.gradle.kts
@@ -18,7 +18,6 @@ dependencies {
         api(project(":libcommon-json-jsoniter"))
         api(project(":libcommon-json-jsoniter-kotlin"))
         api(project(":libcommon-jwt"))
-        api(project(":libcommon-jwt-kotlin"))
         api(project(":libcommon-prometheus-client"))
         api(project(":libcommon-r2dbc"))
         api(project(":libcommon-r2dbc-kotlin"))
